0f674a6ff1ff1608ba165af599dd948fbae2a4a2
developer05-hubx <developer05-hubx@gmail.com>
2022-03-30 16:15:21 +0530

    my commit

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+import ProductComponent from "./ProductComponent";
+
+const ProductPage = () => {
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  
+
+  const fetchProducts = async () => {
+     axios
+      .get("https://dummyjson.com/products").then(res=>{
+        console.log("api res:",res);
+        dispatch(setProducts(res.data.products))
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+    // dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products :", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductPage;
